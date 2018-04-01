bf354a677ea853469da26c7f9f8f3dcde5c2dfdb
Brian <justmats@gmail.com>
2018-03-31 23:00:59 -0400

    Style game

--- js/colorGame.js
+++ js/colorGame.js
@@ -51,7 +51,7 @@ resetButton.addEventListener("click", function() {
     squares[i].style.backgroundColor = colors[i];
   }
 
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
